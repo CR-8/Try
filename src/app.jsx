af6e25ec1d11b61d15553958fe54230c6b25e6d4
Pratyush <pratyushtiwari446@gmail.com>
2024-12-29 00:56:17 +0530

    Commit 2.1

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Router from './Components/Router.jsx'
+
+function App() {
+    return (          
+       <div >
+            <Router/>
+            
+        </div>
+    );
+}
+
+export default App;
+
+
